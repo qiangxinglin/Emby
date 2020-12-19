5b70aa3c37da1f0ddf6ee113deabba23cf2c6dd3
Xinglin Qiang <xinglinqiang@gmail.com>
2020-12-20 03:56:29 +0800

    add notify util

--- QuantumultX/Emby_crack.js
+++ QuantumultX/Emby_crack.js
@@ -1,3 +1,14 @@
+/*
+
+************************
+QuantumultX :
+************************
+
+[rewrite_remote]
+https://raw.githubusercontent.com/qiangxinglin/Emby_Crack/main/QuantumultX/Emby_crack.conf, tag=Emby Premiere, update-interval=86400, opt-parser=false, enabled=true
+
+*/
+
 var url = $request.url;
 
 const myStatus = "HTTP/1.1 200 OK";
@@ -51,4 +62,77 @@ const myResponse = {
     body: myData // Optional.
 };
 
-$done(myResponse);
+var $util = util();
+$util.notify('Emby Premiere 已激活');
+$util.done(myResponse);
+
+function util() {
+    const isRequest = typeof $request != "undefined"
+    const isSurge = typeof $httpClient != "undefined"
+    const isQuanX = typeof $task != "undefined"
+    const notify = (title, subtitle = '', message = '') => {
+        if (isQuanX) $notify(title, subtitle, message)
+        if (isSurge) $notification.post(title, subtitle, message)
+    }
+    const write = (value, key) => {
+        if (isQuanX) return $prefs.setValueForKey(value, key)
+        if (isSurge) return $persistentStore.write(value, key)
+    }
+    const read = (key) => {
+        if (isQuanX) return $prefs.valueForKey(key)
+        if (isSurge) return $persistentStore.read(key)
+    }
+    const adapterStatus = (response) => {
+        if (response) {
+            if (response.status) {
+                response["statusCode"] = response.status
+            } else if (response.statusCode) {
+                response["status"] = response.statusCode
+            }
+        }
+        return response
+    }
+    const get = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = {
+                url: options,
+                method: "GET"
+            }
+            $task.fetch(options).then(response => {
+                callback(null, adapterStatus(response), response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) $httpClient.get(options, (error, response, body) => {
+            callback(error, adapterStatus(response), body)
+        })
+    }
+    const post = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = {
+                url: options,
+                method: "POST"
+            }
+            $task.fetch(options).then(response => {
+                callback(null, adapterStatus(response), response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) {
+            $httpClient.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+    }
+    const done = (value = {}) => {
+        if (isQuanX) return $done(value)
+        if (isSurge) isRequest ? $done(value) : $done()
+    }
+    return {
+        isRequest,
+        notify,
+        write,
+        read,
+        get,
+        post,
+        done
+    }
+};
